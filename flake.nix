9d916fc67318a5c0dca344b9fdb93f006ba10d90
Michal Sojka <michal.sojka@cvut.cz>
2025-11-10 12:25:20 +0100

    Don't use URL literal for flake input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "novaboot";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
